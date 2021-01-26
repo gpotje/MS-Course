616917a15d910ce6f44517ff3c41916d3bee6cc8
gpotje <gpotje.souza@gmail.com>
2021-01-26 10:09:33 -0300

    config server  created

---
+++ hr-zuul/src/test/java/com/gpotdesevo/hrapizuul/HrZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpotdesevo.hrapizuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
