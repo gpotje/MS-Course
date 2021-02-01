401ad29567fda1e9bbb1df416a967e28f1adf74f
gpotje <gpotje.souza@gmail.com>
2021-02-01 20:23:18 -0300

    project hr-oauth created

---
+++ hr-oauth/src/test/java/com/gpotdesevo/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpotdesevo.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
